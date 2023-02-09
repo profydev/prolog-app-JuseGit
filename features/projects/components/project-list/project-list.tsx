0757028a8f9fc99c6fe3786426d1a61738bc572f
JuseGit <73843515+JuseGit@users.noreply.github.com>
2023-02-09 17:21:07 +0100

    Add error screen with try again button. (#11)

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import styled from "styled-components";
-import { breakpoint, color, space, theme } from "@styles/theme";
+import { breakpoint, color, space, textFont, theme } from "@styles/theme";
 import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
 import LoadingIcon from "./loading-icon";
@@ -19,8 +20,54 @@ const List = styled.ul`
   }
 `;
 
+const ErrorScreen = styled.div.attrs({
+  "data-cy": "error-screen",
+})`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${color("error", 25)};
+  border: 1px solid ${color("error", 300)};
+  border-radius: ${space(2)};
+  padding: ${space(4)};
+  position: relative;
+  gap: ${space(3)};
+`;
+
+const ErrorContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1;
+  color: ${color("error", 700)};
+  gap: ${space(3)};
+  ${textFont("sm", "medium")};
+`;
+
+const Icon = styled.img`
+  width: ${space(5)};
+  height: ${space(5)};
+`;
+
+const TryAgainButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  border: none;
+  color: ${color("error", 700)};
+  background: transparent;
+  padding: 0;
+  gap: ${space(3)};
+  ${textFont("sm", "medium")};
+
+  &:hover {
+    background: ${color("error", 50)};
+  }
+`;
+
 export function ProjectList() {
-  const { data, isLoading, isError, error } = useGetProjects();
+  const { data, isLoading, isError, error, refetch } = useGetProjects();
 
   if (isLoading) {
     return (
@@ -34,7 +81,18 @@ export function ProjectList() {
 
   if (isError) {
     console.error(error);
-    return <div>Error: {error.message}</div>;
+    return (
+      <ErrorScreen>
+        <Icon src={"/icons/alert-circle.svg"} alt="error icon" />
+        <ErrorContent>
+          <div>There was a problem while loading the project data</div>
+          <TryAgainButton onClick={() => refetch()}>
+            <div>Try again</div>
+            <Icon src={"/icons/arrow-right.svg"} alt="arrow right icon" />
+          </TryAgainButton>
+        </ErrorContent>
+      </ErrorScreen>
+    );
   }
 
   return (
